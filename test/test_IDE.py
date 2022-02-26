13f370789a9caeefcadfa9ea5d938ada6d1d8bbc
isriom <62965230+isriom@users.noreply.github.com>
2022-02-26 15:45:58 -0600

    Create a Log file with the test execution

--- test/test_IDE.py
+++ test/test_IDE.py
@@ -1,6 +1,14 @@
+import logging
+from datetime import datetime
+LOG_NAME='LOG_'+datetime.now()+'.log'
+logging.basicConfig(filename=LOG_NAME, format='%(levelname)s; %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %I:%M %p')
 def func(x):
+    logging.info('Function func exec')
     return true
 
 
 def test_template():
+    logging.info('Test test_template exec')
     assert func(1) == true
+    logging.info('Test test_template finished.')
+    
