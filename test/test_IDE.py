50ce6878c111243059cbc629c473c7cd30c8780a
isriom <62965230+isriom@users.noreply.github.com>
2022-02-28 19:57:35 -0600

    Pytest configuration test

--- test/test_IDE.py
+++ test/test_IDE.py
@@ -1,14 +1,30 @@
 import logging
 from datetime import datetime
-LOG_NAME='LOG_'+datetime.now()+'.log'
-logging.basicConfig(filename=LOG_NAME, format='%(levelname)s; %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %I:%M %p')
-def func(x):
-    logging.info('Function func exec')
-    return true
+
+LOG_NAME = '../logs/LOG_' + datetime.strftime(datetime.now(), '%Y %m %d %H %M') + '.log'
+logging.basicConfig(filename=LOG_NAME, format='%(levelname)s; %(message)s', level=logging.DEBUG,
+                    datefmt='%m/%d/%Y %I:%M %p')
 
 
-def test_template():
-    logging.info('Test test_template exec')
-    assert func(1) == true
-    logging.info('Test test_template finished.')
-    
+##Decorator for test and functions (not in use)##
+def print_test(func):
+	logging.info('Test ' + func.__name__ + ' exec')
+	func()
+	logging.info('Test ' + func.__name__ + ' finished.')
+
+
+def print_func(func):
+	logging.info('Function ' + func.__name__ + ' exec')
+	func()
+	logging.info('Function ' + func.__name__ + ' exec')
+
+
+##Test template##
+def inc(x):
+	return x + 1
+
+
+def test_inc():
+	assert inc(3) == 5
+
+##IDE Test##
