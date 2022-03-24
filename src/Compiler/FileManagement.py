c602654f551e3cffee8fdb9b8a1be402c1e542d6
Bryang2303 <bryang2303@gmail.com>
2022-03-23 21:02:40 -0600

    File management implementation

---
+++ src/Compiler/FileManagement.py
@@ -0,0 +1,22 @@
+def LoadCode(fileName):
+        path=fileName+".txt"#path
+        file=open(path)#abrir
+        content=file.readlines()#lectura de las lineas
+        file.close()#cerrar
+        return content
+
+def SaveCode(fileName,dato):
+        path=fileName+".txt"
+        with open(path, 'r+') as f:
+            f.truncate(0)
+        file=open(path,"a") #a->append
+        file.write(dato+"\n") # escribe el dato en el file
+        file.close()
+
+
+SaveCode("z","SET @var1, True\nIf\n     @var1.Neg")
+saved = LoadCode("z")
+print(saved)
+
+#SaveCode("z","")
+#SaveCode("z","")
